11e0b5e27a7ca9ac1143542011e11cda5fa20e67
Kip Parker <kip@kipparker.co.uk>
2015-01-06 12:15:06 +0000

    Missed one

--- genericadmin/admin.py
+++ genericadmin/admin.py
@@ -103,7 +103,7 @@ class BaseGenericModelAdmin(object):
                 'popup_var': IS_POPUP_VAR,
             }
             resp = json.dumps(data, ensure_ascii=False)
-            return HttpResponse(resp, mimetype='application/json')
+            return HttpResponse(resp, content_type='application/json')
         return HttpResponseNotAllowed(['GET'])
 
     def generic_lookup(self, request):
