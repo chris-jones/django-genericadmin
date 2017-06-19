73a04854d8302b1745928b1c8f4a215763a1662a
Konstantin Seleznev <Seleznev.nvkz@gmail.com>
2017-06-19 18:48:08 +0300

    Update admin.py
    for support on change page

--- genericadmin/admin.py
+++ genericadmin/admin.py
@@ -89,11 +89,13 @@ class BaseGenericModelAdmin(object):
         
         custom_urls = [
             url(r'^obj-data/$', wrap(self.generic_lookup), name='admin_genericadmin_obj_lookup'),
+            url(r'^(.+)/obj-data/$', wrap(self.generic_lookup), name='admin_genericadmin_obj_lookup'),
             url(r'^genericadmin-init/$', wrap(self.genericadmin_js_init), name='admin_genericadmin_init'),
+            url(r'^(.+)/genericadmin-init/$', wrap(self.genericadmin_js_init), name='admin_genericadmin_init'),
         ]
         return custom_urls + super(BaseGenericModelAdmin, self).get_urls()
             
-    def genericadmin_js_init(self, request):
+    def genericadmin_js_init(self, request, var=None):
         if request.method == 'GET':
             obj_dict = {}
             for c in ContentType.objects.all():
@@ -115,7 +117,7 @@ class BaseGenericModelAdmin(object):
             return HttpResponse(resp, content_type='application/json')
         return HttpResponseNotAllowed(['GET'])
     
-    def generic_lookup(self, request):
+    def generic_lookup(self, request, var=None):
         if request.method != 'GET':
             return HttpResponseNotAllowed(['GET'])
         
